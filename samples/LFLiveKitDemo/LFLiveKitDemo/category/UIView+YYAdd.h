96d7fff50988f4a52e8f018e91f45c5f8ed7fd2f
Superbil <superbil@gmail.com>
2017-11-30 04:18:53 +0800

    Fix comment warning
    - change //< to //

--- samples/LFLiveKitDemo/LFLiveKitDemo/category/UIView+YYAdd.h
+++ samples/LFLiveKitDemo/LFLiveKitDemo/category/UIView+YYAdd.h
@@ -52,17 +52,17 @@
  */
 @property (nonatomic, readonly,nullable) UIViewController *viewController;
 
-@property (nonatomic) CGFloat left;    ///< Shortcut for frame.origin.x.
-@property (nonatomic) CGFloat top;     ///< Shortcut for frame.origin.y
-@property (nonatomic) CGFloat right;   ///< Shortcut for frame.origin.x + frame.size.width
-@property (nonatomic) CGFloat bottom;  ///< Shortcut for frame.origin.y + frame.size.height
-@property (nonatomic) CGFloat width;   ///< Shortcut for frame.size.width.
-@property (nonatomic) CGFloat height;  ///< Shortcut for frame.size.height.
-@property (nonatomic) CGFloat centerX; ///< Shortcut for center.x
-@property (nonatomic) CGFloat centerY; ///< Shortcut for center.y
-@property (nonatomic) CGPoint origin;  ///< Shortcut for frame.origin.
-@property (nonatomic) CGSize size;     ///< Shortcut for frame.size.
-@property (nonatomic, readonly) CGRect screenFrame; ///< View frame on the screen, taking into account scroll views.
+@property (nonatomic) CGFloat left;    ///  Shortcut for frame.origin.x.
+@property (nonatomic) CGFloat top;     ///  Shortcut for frame.origin.y
+@property (nonatomic) CGFloat right;   ///  Shortcut for frame.origin.x + frame.size.width
+@property (nonatomic) CGFloat bottom;  ///  Shortcut for frame.origin.y + frame.size.height
+@property (nonatomic) CGFloat width;   ///  Shortcut for frame.size.width.
+@property (nonatomic) CGFloat height;  ///  Shortcut for frame.size.height.
+@property (nonatomic) CGFloat centerX; ///  Shortcut for center.x
+@property (nonatomic) CGFloat centerY; ///  Shortcut for center.y
+@property (nonatomic) CGPoint origin;  ///  Shortcut for frame.origin.
+@property (nonatomic) CGSize size;     ///  Shortcut for frame.size.
+@property (nonatomic, readonly) CGRect screenFrame; ///  View frame on the screen, taking into account scroll views.
 
 /**
  Returns the visible alpha on screen, taking into account superview and window.
